a11a2a21cbfcbdd4933b0a90cc8e33c5f970a588
Robot Machete <229178385@qq.com>
2026-01-26 11:36:54 +0800

    styles: use different app icon and tray icon

--- scripts/Update-Icons.ps1
+++ scripts/Update-Icons.ps1
@@ -12,10 +12,6 @@ $SourceAppIcon = Join-Path $VisualResourcesDir $appIconName
 $trayIconName = "tray_icon.ico"
 $SourceTrayIcon = Join-Path $VisualResourcesDir $trayIconName
 
-# Use app icon as tray icon
-# Should delete this line using a different icon
-Copy-Item -Path $SourceAppIcon -Destination $SourceTrayIcon -Force
-
 # Target paths
 $ProjectDir = Join-Path $PSScriptRoot  "../apps/dogear"
 $AssetsDir = Join-Path $ProjectDir "assets/system"
@@ -30,6 +26,10 @@ if (!(Test-Path $SourceAppIcon)) {
     Write-Error "Error: $appIconName not found at at $VisualResourcesDir"
     exit
 }
+if (!(Test-Path $SourceTrayIcon)) {
+    Write-Error "Error: $trayIconName not found at at $VisualResourcesDir"
+    exit
+}
 
 # Create the target directory if it doesn't exist
 if (!(Test-Path $AssetsDir)) {
@@ -40,7 +40,7 @@ if (!(Test-Path $AssetsDir)) {
 # Flutter assets
 Write-Host "Updating Flutter Assets Icon..." -ForegroundColor Cyan
 Copy-Item -Path $SourceAppIcon -Destination $TargetAssetsAppIcon -Force
-Copy-Item -Path $SourceAppIcon -Destination $TargetAssetsTrayIcon -Force
+Copy-Item -Path $SourceTrayIcon -Destination $TargetAssetsTrayIcon -Force
 
 # Windoes resources
 Write-Host "Updating Windows Native Icon..." -ForegroundColor Cyan
