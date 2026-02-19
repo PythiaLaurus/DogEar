fe98f622414242cc74260b2ee95df729bf031d05
Robot Machete <229178385@qq.com>
2026-02-19 23:18:27 +0800

    refactor(UserPrefsDefaults): move it to the same file of [UserPreferencesState]

--- apps/dogear/lib/features/settings/domain/user_preferences_state.dart
+++ apps/dogear/lib/features/settings/domain/user_preferences_state.dart
@@ -1,7 +1,8 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hotkey_manager/hotkey_manager.dart';
 
-import 'user_prefs_defaults.dart';
+import 'package:flutter/services.dart' show LogicalKeyboardKey;
+import 'package:hotkey_manager/hotkey_manager.dart' show HotKey, HotKeyModifier;
 
 part 'user_preferences_state.freezed.dart';
 part 'user_preferences_state.g.dart';
@@ -21,4 +22,15 @@ abstract class UserPreferencesState with _$UserPreferencesState {
 
   factory UserPreferencesState.fromJson(Map<String, dynamic> json) =>
       _$UserPreferencesStateFromJson(json);
+}
+
+final class UserPrefsDefaults {
+  static final shortcutDefault = HotKey(
+    key: LogicalKeyboardKey.home,
+    modifiers: [HotKeyModifier.control, HotKeyModifier.alt],
+  );
+  static const dogEarColorArgbDefault = 0xFF8F4C33;
+  static const closeToTrayDefault = true;
+  static const showTrayIconDefault = true;
+  static const autostartDefault = false;
 }
